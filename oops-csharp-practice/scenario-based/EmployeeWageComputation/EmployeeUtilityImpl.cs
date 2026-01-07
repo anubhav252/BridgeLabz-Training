80c199cce84f4a9908b90636078b4392dd1b60b7
anubhav252 <yanubhav977@gmail.com>
2026-01-07 14:49:47 +0530

    [Anubhav Yadav] Add. day 16 UC1 Employee Wage Computation

---
+++ oops-csharp-practice/scenario-based/EmployeeWageComputation/EmployeeUtilityImpl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmployeeUtilityImpl:IEmployee
+    {
+        private List<Employee> employees = new List<Employee>();
+        Random forAttendance = new Random();
+        private Employee employee;
+        public Employee AddEmployee()
+        {
+            employee = new Employee();
+            Console.WriteLine("Enter Employee Id : ");
+            employee.EmployeeId = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee name :");
+            employee.EmployeeName = Console.ReadLine();
+
+            employees.Add(employee);
+            return employee;    
+        }
+
+        public void EmployeeAttendance()
+        {
+            if (employees.Count != 0)
+            {
+                long noChecker=forAttendance.Next(1,100000);
+                Console.WriteLine(noChecker);
+                Console.WriteLine("Enter the no. on your screen : ");
+                long employeeInput=long.Parse(Console.ReadLine());
+          
+                if (employeeInput == noChecker)
+                {
+                    Console.WriteLine("Present");
+                }
+                else
+                {
+                    Console.WriteLine("Absent");
+                }
+            }
+            else {
+                Console.WriteLine("Add employee first "); 
+            }
+        }
+
+        public void DisplayDetails()
+        {
+            foreach(Employee emp in employees)
+            {
+                Console.WriteLine("Employee Id : " + emp.EmployeeId + "\nEmployee Name : " + emp.EmployeeName + "\nEmployee Salary : " + emp.EmployeeSalary );
+            }   
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/EmployeeWageComputation/IEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal interface IEmployee
+    {
+        Employee AddEmployee();
+        void EmployeeAttendance();
+
+        void DisplayDetails();
+    }
+}
