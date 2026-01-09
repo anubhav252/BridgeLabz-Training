ee700a11e571a4107bf445c324c851cca3bef440
anubhav252 <yanubhav977@gmail.com>
2026-01-09 15:55:29 +0530

    [Anubhav Yadav] Refactor. day 18 scenario based problem atm dispenser added scenario B and scenario C

--- data-structures-csharp-practice/scenario-based/AtmDispenser.cs
+++ data-structures-csharp-practice/scenario-based/AtmDispenser.cs
@@ -12,6 +12,7 @@ namespace Fitness_Tracker
         {
             int noOfNotes = 0;
             int[] notes = { 1, 2, 5, 10, 20, 50, 100, 200, 500 }; // Scenario A
+            //int[] notes = { 1, 2, 5, 10, 20, 50, 100, 200 }; // Scenario B : temporarily removed 500 rupee note
             for (int i = notes.Length - 1; i >= 0; i--)
             {
                 if (amount!=0)
@@ -22,10 +23,29 @@ namespace Fitness_Tracker
                 }
             }
             Console.WriteLine("total no of notes : "+noOfNotes);
-
         }
-        
+
+
+        public void FallBackCombo(int amount) { // Scenario C: Display fallback combo if exact change isn’t possible.
+            int[] notes = { 200,100 };
+            int hasAmount = 0;
+            int originalAmount = amount;
+            for (int i = 0; i < notes.Length; i++)
+            {
+                int noOfNotes = amount / notes[i];
+                if (noOfNotes > 0)
+                {
+                    hasAmount += noOfNotes * notes[i];
+                    amount-= noOfNotes * notes[i];
+                }
+            }
+            if (hasAmount != originalAmount)
+            {
+                Console.WriteLine("only " + hasAmount + " can be withdrawn don't have changes");
+            }
+        }
     }
+
     internal class AtmMain
     {
         static void Main(string[] args)
@@ -33,6 +53,7 @@ namespace Fitness_Tracker
             int amount = int.Parse(Console.ReadLine());
             AtmDispenser atm = new AtmDispenser();
             atm.GetNotes(amount);
+            atm.FallBackCombo(amount);  
         }
 
     }
