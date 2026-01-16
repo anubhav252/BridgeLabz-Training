2986c171d4ec7f7e82dd5cd6c904209ceeeb839c
anubhav252 <yanubhav977@gmail.com>
2026-01-16 10:30:07 +0530

    [Anubhav Yadav] Add. browserbuddy tab history manager problem

---
+++ data-structures-csharp-practice/scenario-based/BrowserBuddy/BrowserManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserBuddy
+{
+    internal class BrowserManager
+    {
+        private Stack<BrowserTab> closedTabs = new Stack<BrowserTab>();
+        private BrowserTab currentTab = new BrowserTab();
+
+        public void Visit(string url)
+        {
+            currentTab.Visit(url);
+        }
+
+        public void Back() => currentTab.Back();
+        public void Forward() => currentTab.Forward();
+
+        public void CloseTab()
+        {
+            closedTabs.Push(currentTab);
+            currentTab = new BrowserTab();
+            Console.WriteLine("Tab closed");
+        }
+
+        public void RestoreTab()
+        {
+            if (closedTabs.Count > 0)
+            {
+                currentTab = closedTabs.Pop();
+                Console.WriteLine("Tab restored");
+            }
+            else
+                Console.WriteLine("No closed tabs to restore");
+        }
+    }
+
+}
---
+++ data-structures-csharp-practice/scenario-based/BrowserBuddy/History.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserBuddy
+{
+    internal class HistoryNode
+    {
+        public string Url;
+        public HistoryNode Prev;
+        public HistoryNode Next;
+
+        public HistoryNode(string url)
+        {
+            Url = url;
+        }
+    }
+
+}
