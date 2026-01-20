b972f5d0a6f98fe02576ad82a428abb6785db89c
anubhav252 <yanubhav977@gmail.com>
2026-01-20 16:01:20 +0530

    [Anubhav Yadav] Add. scenario based problem PasswordCracker used bracktracking to find the password

---
+++ data-structures-csharp-practice/scenario-based/PasswordCracker.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Text;
+namespace PasswordCracker
+{
+    class Password
+    {
+        static char[] character={'c','d','e'};
+        static string target = "ecd";
+        static bool found = false;
+
+        public void GetPassword(int targetLength)
+        {
+            StringBuilder currentPassword=new StringBuilder();
+            GeneratePassword(0,targetLength,currentPassword);
+
+        }
+        public void GeneratePassword(int index,int targetLength,StringBuilder currentPassword)
+        {
+            if (found)
+            {
+                return;
+            }
+            if (index == targetLength)
+            {
+                System.Console.WriteLine(currentPassword.ToString()+" , ");
+                if(currentPassword.ToString() == target)
+                {
+                    System.Console.WriteLine("Password found: " + currentPassword.ToString());
+                    found = true;
+                }
+                return;
+            }
+            
+            for (int i = 0; i < character.Length; i++)
+            {
+                currentPassword.Append(character[i]);
+                GeneratePassword(index + 1, targetLength, currentPassword);
+                currentPassword.Remove(currentPassword.Length - 1, 1);
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Password password = new Password();
+            password.GetPassword(3);
+        }
+    }
+}
