edbe5ead57d55ab37d03d42e1d622d9eddefe1a8
anubhav252 <yanubhav977@gmail.com>
2026-01-23 23:07:06 +0530

    [Anubhav Yadav] Add. csharp exceptions problem

---
+++ collections-csharp-practice/gcr-codebase/csharp-Exceptions/FileNotFoundException.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+
+class FileNotFoundException
+{
+    static void Main()
+    {
+        string fileName = "data.txt";
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string content = reader.ReadToEnd();
+                Console.WriteLine(content);
+            }
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("File not found");
+        }
+    }
+}
